74cd22a467eba644cb57d1013345677eca4e99fe
Simone Gentili <sensorario@gmail.com>
2018-10-17 10:48:17 +0200

    project setup and env variables

--- test/helper.js
+++ test/helper.js
@@ -1,30 +1,49 @@
 'use strict'
 
-// This file contains code that we reuse
-// between our tests.
+// supporto nativo per le promises
 
 const Fastify = require('fastify')
 const fp = require('fastify-plugin')
 const App = require('../app')
 
-// Fill in this config with all the configurations
-// needed for testing the application
+const clean = require('mongo-clean')
+const { MongoClient } = require('mongodb')
+const { beforeEach, tearDown } = require('tap')
+const url = 'mongodb://localhost:27017'
+const database = 'tests'
+
+let client
+
+beforeEach(async function () {
+  if (!client) {
+    client = await MongoClient.connect(url, {
+      w: 1,
+      useNewUrlParser: true
+    })
+  }
+  await clean(client.db(database))
+})
+
+tearDown(async function () {
+  if (client) {
+    await client.close()
+    client = null
+  }
+})
+
 function config () {
-  return {}
+  return {
+    mongodb: {
+      client,
+      database
+    }
+  }
 }
 
-// automatically build and tear down our instance
 function build (t) {
   const app = Fastify()
-
-  // fastify-plugin ensures that all decorators
-  // are exposed for testing purposes, this is
-  // different from the production setup
   app.register(fp(App), config())
-
-  // tear down our app after we are done
   t.tearDown(app.close.bind(app))
-
   return app
 }
 
