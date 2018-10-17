8a1020fe4de14b8de082fd124d74b53778746202
Simone Gentili <sensorario@gmail.com>
2018-10-17 10:29:21 +0200

    add mongo configuration

--- test/services/root.test.js
+++ test/services/root.test.js
@@ -3,25 +3,11 @@
 const { test } = require('tap')
 const { build } = require('../helper')
 
-test('default root route', (t) => {
-  t.plan(2)
+test('default root route', async (t) => {
   const app = build(t)
 
-  app.inject({
+  const res = await app.inject({
     url: '/'
-  }, (err, res) => {
-    t.error(err)
-    t.deepEqual(JSON.parse(res.payload), { root: true })
   })
+  t.deepEqual(JSON.parse(res.payload), { root: true })
 })
-
-// It you prefer async/await, use the following
-//
-// test('default root route', async (t) => {
-//   const app = build(t)
-//
-//   const res = await app.inject({
-//     url: '/'
-//   })
-//   t.deepEqual(JSON.parse(res.payload), { root: true })
-// })
